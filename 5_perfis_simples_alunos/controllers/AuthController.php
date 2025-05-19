44467bd72eb00084cf8e24c1b9db94c5227f86ce
iiim-lele <leticia.g.souza9@aluno.senai.br>
2025-05-19 11:19:34 -0300

    AuthController
    finalizado

---
+++ 5_perfis_simples_alunos/controllers/AuthController.php
@@ -0,0 +1,42 @@
+<!-- Este arquivo será o responsável por: (Controlar a autenticação) -->
+
+<?php
+class AuthController {
+
+    // Exibe a página de login
+
+public function login($erro = '') {
+    require_once 'views/login.php';
+    renderizarLogin('$erro');
+}
+
+// Processa a tentativa de login
+
+public function autenticar() {
+    $usuario = $_POST['usuario'] ?? '';
+    $senha = $_POST['senha'] ?? '';
+
+    if (empty($usuario) || empty($senha)) {
+        $this ->login('Preencha todos os campos');
+        return; 
+    }
+
+    $dadosUsuario = Auth::autenticar($usuario, $senha);
+
+    if ($dadosUsuario) {
+        Auth::iniciarSessao($dadosUsuario);
+        header('Location: index.php?pagina=lista');
+        exit;
+    } else {
+        $this->login('Usuário ou senha incorretos');
+    }
+}
+
+// Processa o logout
+
+public function logout() {
+    Auth::encerrarSessao();
+    header('Location:index.php?pagina=login');
+    exit;
+}
+}
